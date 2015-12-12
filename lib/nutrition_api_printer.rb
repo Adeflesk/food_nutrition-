1c0e56a32616c3cc42f1adda1f03e95d9ed92aa0
Adrian Corsini <glenflesk@gmail.com>
2015-12-12 11:12:11 +0000

    created food_list test

--- lib/nutrition_api_printer.rb
+++ lib/nutrition_api_printer.rb
@@ -12,10 +12,21 @@ class NutritionApiPrinter
 
   def get_food(food, max,offset)
       data = @na.get_food(food,max,offset)
-      File.open("data/search.json","w")do |f|
-        f.write(JSON.pretty_generate(data))
-      end
+      write_file("data/search.json",data)
       return data
+  end
+
+  def get_food_list(type,sort,max,offset)
+    data = @na.get_food_list(type,sort, max,offset)
+    write_file("data/food_list.json",data)
+    return data
+  end
+
+
+  def write_file(file,data)
+  File.open(file,"w")do |f|
+        f.write(JSON.pretty_generate(data))
+  end
   end
 
 
@@ -26,3 +37,4 @@ naa = NutritionApiAdapter.new(SetupParams.new)
 np = NutritionApiPrinter.new(naa)
 
 np.get_food("Cod",1,0)
+np.get_food_list("f","n",2,0)
--- spec/lib/nutrition_api_printer_spec.rb
+++ spec/lib/nutrition_api_printer_spec.rb
@@ -36,5 +36,37 @@ describe NutritionApiPrinter do
       expect(Dir['data/*'].empty?).to be_falsey
       expect(JSON.parse(File.read('data/search.json'))).to eq(test_json)
     end
+
+    it "should create a food_list  for a food"do
+      naa = instance_double("NutritionApiAdapter")
+      test_json = JSON.parse('{
+                                  "list": {
+                                    "lt": "f",
+                                    "start": 0,
+                                    "end": 2,
+                                    "total": 2,
+                                    "sr": "28",
+                                    "sort": "n",
+                                    "item": [
+                                      {
+                                        "offset": 0,
+                                        "id": "09427",
+                                        "name": "Abiyuch, raw"
+                                      },
+                                      {
+                                        "offset": 1,
+                                        "id": "09002",
+                                        "name": "Acerola juice, raw"
+                                      }
+                                    ]
+                                  }
+}')
+
+      allow(naa).to receive(:get_food_list).and_return(test_json)
+      np = NutritionApiPrinter.new(naa)
+      np.get_food_list("f",'n',2,0)
+      expect(JSON.parse(File.read('data/food_list.json'))).to eq(test_json)
+
+    end
   end
 end
